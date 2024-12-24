2d391eb729acce9cc980880668bf6f1b620d0674
Krishnavir-lv <krishnavir.lv@gmail.com>
2024-12-24 18:37:29 +0530

    constant

--- src/Constant/Api.js
+++ src/Constant/Api.js
@@ -1,2 +1,2 @@
-//  export const BASE_URL = 'http://165.22.212.175:3001/admin/V1';
-export const BASE_URL = 'http://192.168.1.7:3001/admin/v1';
+ export const BASE_URL = 'http://165.22.212.175:3001/admin/V1';
+// export const BASE_URL = 'http://192.168.1.7:3001/admin/v1';
