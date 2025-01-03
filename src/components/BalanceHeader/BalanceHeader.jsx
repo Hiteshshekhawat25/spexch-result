8be1e6ebdc18232aa76bcfcd264b07bd99e70ed0
Krishnavir-lv <krishnavir.lv@gmail.com>
2025-01-03 12:22:22 +0530

    balance header fix

--- src/components/BalanceHeader/BalanceHeader.jsx
+++ src/components/BalanceHeader/BalanceHeader.jsx
@@ -1,23 +1,24 @@
-
-
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setBalanceData } from "../../Store/Slice/balanceSlice";
-import { getBalanceData } from "../../Services/Downlinelistapi"; 
+import { getBalanceData } from "../../Services/Downlinelistapi";
 
 const BalanceHeader = () => {
   const dispatch = useDispatch();
   const balanceData = useSelector((state) => state.balance);
+  console.log("balanceData", balanceData);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getBalanceData("user/user-data-summary"); 
+        const response = await getBalanceData("user/user-data-summary");
         if (response?.data?.success) {
           dispatch(setBalanceData(response.data.data));
         } else {
-          console.error("Failed to fetch balance data: ", response.data?.message);
+          console.error(
+            "Failed to fetch balance data: ",
+            response.data?.message
+          );
         }
       } catch (error) {
         console.error("Failed to fetch balance data:", error.message);
@@ -51,30 +52,33 @@ const BalanceHeader = () => {
         <div className="text-left py-1 text-gray-500 text-xs font-medium pl-2"></div>
       </div>
       <div className="grid grid-cols-7 divide-x divide-gray-200">
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.totalBalance}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP {new Intl.NumberFormat("en-IN").format(balanceData.totalBalance)}
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.totalExposure}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP{" "}
+          <span className="text-red-800">
+            ({new Intl.NumberFormat("en-IN").format(balanceData.totalExposure)})
+          </span>
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.availableBalance}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP{" "}
+          {new Intl.NumberFormat("en-IN").format(balanceData.availableBalance)}
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.balance}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP {new Intl.NumberFormat("en-IN").format(balanceData.balance)}
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.totalavailbalance}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP{" "}
+          {new Intl.NumberFormat("en-IN").format(balanceData.totalavailbalance)}
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2">
-          {balanceData.uplinePL}
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2">
+          IRP {new Intl.NumberFormat("en-IN").format(balanceData.uplinePL)}
         </div>
-        <div className="text-left py-1 text-blue-800 text-sm font-semibold pl-2"></div>
+        <div className="text-left py-1 text-blue-800 text-sm font-bold pl-2"></div>
       </div>
     </div>
   );
 };
 
 export default BalanceHeader;
-
-
