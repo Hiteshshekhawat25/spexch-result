d3b6a192310bd83f9aef40e63a35292b857c3935
Yeshvi <yeshvi.lv@gmail.com>
2025-01-07 18:24:20 +0530

    setted the current date

--- src/Store/Slice/betListFilterSlice.js
+++ src/Store/Slice/betListFilterSlice.js
@@ -1,10 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Get the current date in 'YYYY-MM-DD' format
+const currentDate = new Date().toISOString().split('T')[0];
+
 const initialState = {
   type: '',
   sport: '',
-  fromDate: '',
-  toDate: '',
+  fromDate: currentDate,  // Set current date as the default fromDate
+  toDate: currentDate,    // Set current date as the default toDate
 };
 
 const betListFilterSlice = createSlice({
@@ -26,8 +29,8 @@ const betListFilterSlice = createSlice({
     resetBetFilters: (state) => {
       state.type = '';
       state.sport = '';
-      state.fromDate = '';
-      state.toDate = '';
+      state.fromDate = currentDate; // Reset to current date
+      state.toDate = currentDate;   // Reset to current date
     },
   },
 });
@@ -37,3 +40,43 @@ export const { setType, setSport, setFromDate, setToDate, resetBetFilters } = be
 export const selectBetListFilter = (state) => state.betListFilter;
 
 export default betListFilterSlice.reducer;
+
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   type: '',
+//   sport: '',
+//   fromDate: '',
+//   toDate: '',
+// };
+
+// const betListFilterSlice = createSlice({
+//   name: 'betListFilter',
+//   initialState,
+//   reducers: {
+//     setType: (state, action) => {
+//       state.type = action.payload;
+//     },
+//     setSport: (state, action) => {
+//       state.sport = action.payload;
+//     },
+//     setFromDate: (state, action) => {
+//       state.fromDate = action.payload;
+//     },
+//     setToDate: (state, action) => {
+//       state.toDate = action.payload;
+//     },
+//     resetBetFilters: (state) => {
+//       state.type = '';
+//       state.sport = '';
+//       state.fromDate = '';
+//       state.toDate = '';
+//     },
+//   },
+// });
+
+// export const { setType, setSport, setFromDate, setToDate, resetBetFilters } = betListFilterSlice.actions;
+
+// export const selectBetListFilter = (state) => state.betListFilter;
+
+// export default betListFilterSlice.reducer;
