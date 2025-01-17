79e95f24d018e6a98628131707fff9e11b4c60de
Krishnavir-lv <krishnavir.lv@gmail.com>
2025-01-17 17:12:53 +0530

    delete

--- src/components/Modal/DeleteConfirmationModal.jsx
+++ src/components/Modal/DeleteConfirmationModal.jsx
@@ -97,8 +97,8 @@ const DeleteConfirmationModal = ({
       <div className="bg-white rounded-lg shadow-lg w-1/3 mt-16 ">
         {" "}
         {/* Add margin-top */}
-        <div className="flex justify-between items-center bg-gradient-blue text-white p-4 rounded-t-lg">
-          <h3 className="text-lg font-semibold">Confirmation</h3>
+        <div className="flex justify-between items-center bg-gradient-blue text-white p-2 rounded-t-lg">
+          <h3 className="text-md font-semibold">Confirmation</h3>
           <button onClick={onClose} className="text-white">
             <MdClose size={24} />
           </button>
